fb79f034f83957cc639451732123123093eb6766
Luis at nexus <luis.pulido@zktechnology.eu>
2017-09-04 21:09:12 +0200

    first commit

---
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/main/LoginPresenter.java
@@ -0,0 +1,67 @@
+package uk.co.ribot.androidboilerplate.ui.main;
+
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
+import uk.co.ribot.androidboilerplate.data.DataManager;
+import uk.co.ribot.androidboilerplate.data.model.Ribot;
+import uk.co.ribot.androidboilerplate.injection.ConfigPersistent;
+import uk.co.ribot.androidboilerplate.ui.base.BasePresenter;
+import uk.co.ribot.androidboilerplate.util.RxUtil;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@ConfigPersistent
+public class LoginPresenter extends BasePresenter<ProvidedPresenterOperations.LoginMvpView> {
+
+    private final DataManager mDataManager;
+    private Subscription mSubscription;
+
+    @Inject
+    public LoginPresenter(DataManager dataManager) {
+        mDataManager = dataManager;
+    }
+
+    @Override
+    public void attachView(ProvidedPresenterOperations.LoginMvpView mvpView) {
+        super.attachView(mvpView);
+    }
+
+    @Override
+    public void detachView() {
+        super.detachView();
+        if (mSubscription != null) mSubscription.unsubscribe();
+    }
+
+    public void loadRibots() {
+        checkViewAttached();
+        RxUtil.unsubscribe(mSubscription);
+        mSubscription = mDataManager.getRibots()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Subscriber<List<Ribot>>() {
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "There was an error loading the ribots.");
+                        getMvpView().showError();
+                    }
+
+                    @Override
+                    public void onNext(List<Ribot> ribots) {
+                        if (ribots.isEmpty()) {
+                            getMvpView().showError();
+                        } else {
+                            getMvpView().showLoginResult();
+                        }
+                    }
+                });
+    }
+
+}
